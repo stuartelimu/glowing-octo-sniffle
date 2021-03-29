860c96bc24bf032feb6bceb44d5eb8eaf0c24bd8
stuartelimu <stuartelimu@gmail.com>
2021-03-29 22:41:47 +0300

    add about page tests

--- pages/tests.py
+++ pages/tests.py
@@ -26,3 +26,26 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get('/')
         self.assertNotContains(response, '<h1>Hello, world!</h1>')
 
+
+class AboutPageTests(SimpleTestCase):
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_view_by_url_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_view_uses_correct_template(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_about_page_contains_correct_html(self):
+        response = self.client.get('/about/')
+        self.assertContains(response, '<h1>About Page</h1>')
+
+    def test_about_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/about/')
+        self.assertNotContains(response, 'Lorem Ipsum')
