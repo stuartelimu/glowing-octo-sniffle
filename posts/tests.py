742b488ad077fcdd1b46badc98a4cf50ae34b846
stuartelimu <stuartelimu@gmail.com>
2021-03-29 23:11:40 +0300

    add tests for posts app

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class PostTests(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='Hello, world!')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'Hello, world!')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('posts'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Hello, world!')
+        self.assertTemplateUsed(response, 'post_list.html')
